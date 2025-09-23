d5e58ef6dfa7215106b9a63692e84cf1b1f91346
manu-gibst <nurasyl.mauletkali@gmail.com>
2025-09-23 22:46:17 +0800

    Initial Setup + Simple POST

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('bodyParser');
+const cors = require('cors');
+const app = express();
+const port = 8080;
+
+app.use(cors())
+app.use(bodyParser.json({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.post('/analyse', (req, res) => {
+    const accels = req.body;
+
+    if (!Array.isArray(accels))
+        return res.status(400).json({ error: 'Request body must be array!' });
+
+    for (let accel of accels) {
+        let missing = '';
+        if (accel.timestamp === undefined) missing += 'timestamp';
+        if (accel.x === undefined) missing += 'x';
+        if (accel.y === undefined) missing += 'y';
+        if (accel.z === undefined) missing += 'z';
+
+        if (missing !== '') {
+            return res.status(400).json(
+                { error: `Following request body parameters are missing in the array: ${missing}` }
+            );
+        }
+    }
+
+    console.log(`Array is accepted: ${accel}`);
+    res.status(200).json({
+        message: 'Array accepted',
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+});
