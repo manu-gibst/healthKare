15025500eb1f88b3e049b4c25de9138437d0ba5f
manu-gibst <nurasyl.mauletkali@gmail.com>
2025-09-23 23:18:10 +0800

    Working button

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,13 +1,44 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+    <div class="row full-width">
+      <div class="col text-center">{{ data.state }}</div>
+    </div>
+    <div class="row">
+      <div class="col text-center">
+        <q-btn
+          round color="primary"
+          :icon="iconName"
+          @click="toggleSleep"
+          size="32px"
+        />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script setup>
-//
+  import {reactive, computed} from 'vue';
+
+  const data = reactive({
+    state: 'initial',
+  });
+
+  const toggleSleep = () => {
+    console.log('toggleSleep called');
+    switch (data.state) {
+      case 'initial':
+        data.state = 'sleeping';
+        break;
+      case 'sleeping':
+        data.state = 'finished';
+        break;
+      default:
+        data.state = 'initial';
+        break;
+    }
+  };
+
+  const iconName = computed(() => {
+    return data.state === 'sleeping'? 'pause' : 'play_arrow'
+  });
 </script>
