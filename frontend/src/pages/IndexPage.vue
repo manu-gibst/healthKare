95d0c090dfafacc4f4c01575dc5ff7f5a74cec6a
manu-gibst <nurasyl.mauletkali@gmail.com>
2025-09-24 12:00:11 +0800

    Start/Stop Tracking motion functions implemented

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -2,6 +2,7 @@
   <q-page class="flex flex-center">
     <div class="row full-width">
       <div class="col text-center">{{ data.state }}</div>
+      <div class="col text-center">{{ data.log }}</div>
     </div>
     <div class="row">
       <div class="col text-center">
@@ -21,19 +22,19 @@
 
   const data = reactive({
     state: 'initial',
+    log: '',
   });
 
-  const toggleSleep = () => {
-    console.log('toggleSleep called');
+  async function toggleSleep () {
     switch (data.state) {
-      case 'initial':
+      case 'not sleeping':
+        await startTracking();
         data.state = 'sleeping';
         break;
       case 'sleeping':
-        data.state = 'finished';
-        break;
       default:
-        data.state = 'initial';
+        await stopTracking();
+        data.state = 'not sleeping';
         break;
     }
   };
@@ -41,4 +42,28 @@
   const iconName = computed(() => {
     return data.state === 'sleeping'? 'pause' : 'play_arrow'
   });
+
+  async function startTracking() {
+    data.log = 'startTracking() called';
+    if (typeof DeviceMotionEvent !== 'undefined' && typeof DeviceMotionEvent.requestPermission === 'function') {
+      const res = await DeviceMotionEvent.requestPermission();
+      if (res !== 'granted') return;
+    }
+    window.addEventListener('devicemotion', onMotion, { passive: true });
+  }
+
+  async function stopTracking() {
+    window.removeEventListener('devicemotion', onMotion);
+  }
+
+  function onMotion(ev) {
+    const a = ev.acceleration || ev.accelerationIncludingGravity;
+    if (!a) return;
+    const x = a.x || 0, y = a.y || 0, z = a.z || 0;
+    const timestamp = Date.now();
+    // Units may be m/s^2. Optionally convert to g: divide by 9.80665.
+    console.log(`{ timestamp=${timestamp}, x=${x}, y=${y}, z=${z} }`);
+    data.log = `{ timestamp=${timestamp}, x=${x}, y=${y}, z=${z} }`;
+    // samples.push({ timestamp, x, y, z });
+  }
 </script>
