95fa40eb6ace6a79dd4980daf5fa9ad4bae74c55
manu-gibst <nurasyl.mauletkali@gmail.com>
2025-09-27 08:34:17 +0800

    cors changed

--- backend/index.js
+++ backend/index.js
@@ -19,11 +19,7 @@ const app = express();
 
 const port = 3000;
 
-const corsOptions = {
-    origin: 'https://manu-gibst-github-io-mokd.vercel.app',
-}
-
-app.use(cors(corsOptions))
+app.use(cors())
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -237,7 +237,8 @@
   */
 
 
-  const baseUrl = 'http://54.196.187.98:3000';
+  // const baseUrl = 'http://54.196.187.98:3000';
+  const baseUrl = 'https://172.20.10.3:3000';
 
   const instance = axios.create({
     baseURL: baseUrl,
